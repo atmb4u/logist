eca06a29bd1d4cccc78fd2083c2c4db2e45e28ae
Anoop Thomas Mathew <atm@profoundis.com>
2016-01-10 11:42:34 +0530

    new changes in tests

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 import random
-from time import sleep
 from datetime import datetime
 from __init__ import Logist
 
@@ -8,10 +7,14 @@ types = ["SUCCESS", "ERROR", "INFO", "WARNING"]
 sub_types = ["ACCESS", "WRITE", "READ", "EDIT", "DELETE"]
 descriptions = ["d1", "d2", "d3", "d4", "d5", "d6"]
 start = datetime.now()
-for log in xrange(100000):
-    # sleep(random.random()/10000)
+log_count = 105500
+for log in xrange(log_count):
     log_type = types[random.randint(0, 3)]
     log_sub_type = sub_types[random.randint(0, 4)]
     description = descriptions[random.randint(0, 5)]
     l.log(log_type, log_sub_type, description)
-print(datetime.now() - start)
+time_delta = datetime.now() - start
+print("Benchmark\n%d requests in %s\n %f logs/second" % (log_count, time_delta, log_count/time_delta.seconds))
+
+
+# TODO - tests for compression switch, memory/file switch, filters, count
